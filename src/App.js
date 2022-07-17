53b50cff8f266808e845ae3b0d396f8045e7c042
SerNicx <nicx901@gmail.com>
2022-07-16 22:22:28 -0300

    añadiendo componente NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Espacio de E-commerce de Sergio Niño
